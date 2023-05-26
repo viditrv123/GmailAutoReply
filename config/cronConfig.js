14f3f069e979b70fa88bfe044a220a8246bfdbad
viditrv123 <vidit.shanaaya.vinaaya@gmail.com>
2023-05-26 09:52:41 +0530

    Set the interval to be dynamic between 45 to 120 second

--- config/cronConfig.js
+++ config/cronConfig.js
@@ -6,7 +6,7 @@ import { delayTimer } from '../helper'
 const enableCrons = async () => {
   console.log('Email Auto Reply cron starting .....')
   let startCron
-  const delay = delayTimer(10, 20)
+  const delay = delayTimer(45, 120)
   const timeout = delay * 1000
   clearInterval(startCron)
   await emailAutoReplyCron()
