cce72e6570bc482d30fe7dba7736a9e1eb8f3aa5
viditrv123 <vidit.shanaaya.vinaaya@gmail.com>
2023-05-26 09:51:52 +0530

    Removed cron time from env variables

--- config/environmentVariables.js
+++ config/environmentVariables.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const ENVIRONMENT_VARIABLE = {
-  EMAIL_AUTO_REPLY_CRON_TIME: process.env.EMAIL_AUTO_REPLY_CRON_TIME,
   SCOPES: (process.env.SCOPES).split('|'),
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
